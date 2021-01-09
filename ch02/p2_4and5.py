6572c2c4ef160c834be02cf894bc720d596633b3
Yeonsil Choi <ychoi1001@gmail.com>
2021-01-08 23:48:42 -0500

    uploading p2.3

---
+++ ch02/p2_4and5.py
@@ -0,0 +1,21 @@
+## Write a program that prompts the user for two integers and then prints
+# - The sum
+# - The difference
+# - The product
+# - The average
+# - The distance (absolute value of the difference)
+# - The maximum (the larger of the two)
+# - The minimum (the smaller of the two) #
+
+# reads two numbers
+num1 = float(input("Input number 1: "))
+num2 = float(input("Input number 2: "))
+
+# display result
+print("%-11s %6d" %("Sum:", num1+num2))
+print("%-11s %6d" %("Difference:", num1-num2))
+print("%-11s %6d" %("Product:", num1*num2))
+print("%-11s %9.2f" %("Average:", (num1+num2)/2))
+print("%-11s %6d" %("Distance:", abs(num1-num2)))
+print("%-11s %6d" %("Maximum:", max(num1,num2)))
+print("%-11s %6d" %("Minimum:", min(num1,num2)))
